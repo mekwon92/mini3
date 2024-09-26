52758ff4c06bcd49e9277e489aab61a6d4a11092
mekwon92 <me92100984@gmail.com>
2024-09-27 00:49:33 +0900

    회원정보 추가

--- src/miniCustomer/CustomerMain.java
+++ src/miniCustomer/CustomerMain.java
@@ -4,19 +4,22 @@ public class CustomerMain {
 	public static void main(String[] args) {
 		CustomerService cs = new CustomerService();
 		
-		cs.login();
-		cs.customerAdd();
-		cs.customerRemove();
-		cs.printCustomer();
+		System.out.println("****얼라딘입니다. 어서오세요****");
+
 		while(true) {
-			int input = MiniUtils.next("1. 로그인 2. 회원가입 3. 종료", Integer.class);
+			int input = MiniUtils.next("1. 로그인 2. 회원가입 3. 비회원 구매 4. 종료", Integer.class ,  t -> t >= 1 && t <= 4, "1에서 4 사이의 수");
 			switch (input) {
 			case 1:
 				cs.login();
+				cs.afterLogin();
+				break;
 			case 2:
 				cs.customerAdd();
 				break;
 			case 3:
+				System.out.println("비회원구매 서비스 예정 첫화면으로 돌아갑니다");
+				break;
+			case 4:
 				System.out.println("감사합니다");
 				return;
 			default:
