53a49ef10b20094a599ed7f4ef00a9604e856b57
hhj8159 <hhj8159@gmail.com>
2024-09-26 17:25:49 +0900

    서적관리_Book, miniPBookService

---
+++ src/mini/miniPBookService.java
@@ -0,0 +1,30 @@
+package mini;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class miniPBookService {
+// 책 목록
+private List bookList = new ArrayList();
+private static final int TMPCOUNT = 1000;
+
+// 초기화 블럭
+{
+	System.out.println("초기 데이터를 삽입합니다.");
+	bookList.add(new Book("000", "홍길동전", "홍길동", "길동사","00-0000-0000","디테일", 10000, 1000, false));
+	System.out.println("초기데이터 삽입 완료.");
+}
+
+public static void main(String[] args) {
+// booklist.forEach(System.out::println);
+// System.out.println());
+}
+
+public void addBook() {	
+	
+}
+// public void
+
+
+
+}
