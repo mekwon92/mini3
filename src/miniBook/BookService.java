0fd4f6b577fb471953d1fa4e244a3f4211a906a7
DaHnDell <khanman1208@gmail.com>
2024-09-28 19:23:33 +0900

    0927_검색부 구현중_1

--- src/miniBook/BookService.java
+++ src/miniBook/BookService.java
@@ -9,6 +9,8 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import miniCustomer.*;
+
 public class BookService {
 // 책 목록
 	private static List<Book> bookList = new ArrayList<Book>();
@@ -55,25 +57,26 @@ public class BookService {
 		pBook.forEach(x -> System.out.print(x + "\n"));
 	}
 	
+	
 	/**
-	 * 저자를 통한 출력부, printBooks 오버로딩
+	 * 리스트를 통한 출력부, printBooks 오버로딩
 	 * @param  
 	 * @author KHM
 	 */
 	public void printBooks(List<Book> listTarget) {
 		System.out.println("*****서적 목록은 다음과 같습니다.*****");
 		List<Book> pBook = new ArrayList<>(listTarget);
-//		Collections.sort(pBook);
+		pBook.sort((p1, p2)-> p1.getBookName().compareTo(p2.getBookName()));
 		pBook.forEach(x -> System.out.print(x + "\n"));
 	}
 
 	/**
-	 * 도서번호를 통한 검색
+	 * 도서번호, ISBN 검색
 	 * 일치하는 경우 한가지밖에 없기 떄문에 추후 예외처리 필요
 	 * @param String bookID
 	 * @author KHM
 	 */
-	public Book findByBookId(String no) { // 도서번호로 검색
+	public Book findByBookId(String no) {
 		Book book = null;
 		for (int i = 0; i < bookList.size(); i++) {
 			if (bookList.get(i).getBookId() == no) {
@@ -82,12 +85,25 @@ public class BookService {
 		}
 		return book;
 	}
+
+	public Book findByBookISBN(String no) {
+		Book book = null;
+		for (int i = 0; i < bookList.size(); i++) {
+			if (bookList.get(i).getISBookNum() == no) {
+				book = bookList.get(i);
+			}
+		}
+		return book;
+	}
+	
 	/**
 	 * 일치하는 저자에 따른 검색결과 반환
 	 * 저자 한명이 다양한 검색결과를 가질 수 있기 때문에 리스트 사용
+	 * 마지막 일치하는 검색 결과가 있을 경우 
 	 * @param String writer
+	 * @author KHM
 	 */
-	public void findByWriter(String writer) { 
+	private void findByWriter(String writer) { 
 		List<Book> bookRes = new ArrayList<Book>();
 		for(int i = 0; i < bookList.size(); i++) {
 			Book book = bookList.get(i);
@@ -99,23 +115,65 @@ public class BookService {
 			System.out.println("SYSTEM :: 저자명 : " + writer + ", 이에 따른 검색결과가 없습니다. ::" );
 		} else {
 			System.out.println("SYSTEM :: 저자명 : " + writer + ", 이에 따른 검색 결과입니다. ::" );
-			
+			printBooks(bookRes);
 		}
-		
+		System.out.println("SYSTEM :: END OF QUERRY");
 	}
-	public Book findByBook(String no) { // 도서번호로 검색
-		Book book = null;
-		for (int i = 0; i < bookList.size(); i++) {
-			if (bookList.get(i).getBookId() == no) {
-				book = bookList.get(i);
+	
+	/**
+	 * 일치하는 제목에 따른 검색결과 반환
+	 * 제목 하나가 출판사에 따른 다양한 검색결과를 가질 수 있기 때문에 리스트 사용
+	 * 마지막 일치하는 검색 결과가 있을 경우 
+	 * @param String writer
+	 * @author KHM
+	 */
+	private void findByName(String bookName) { 
+		List<Book> bookRes = new ArrayList<Book>();
+		for(int i = 0; i < bookList.size(); i++) {
+			Book book = bookList.get(i);
+			if(book.getBookName().equals(bookName)) {
+				bookRes.add(book);
 			}
 		}
-		return book;
+		if(bookRes.isEmpty()) {
+			System.out.println("SYSTEM :: 제목 : " + bookName + ", 이에 따른 검색결과가 없습니다. ::" );
+		} else {
+			System.out.println("SYSTEM :: 제목 : " + bookName + ", 이에 따른 검색 결과입니다. ::" );
+			printBooks(bookRes);
+		}
+		System.out.println("SYSTEM :: END OF QUERRY");
 	}
-
+	
 	public void bookSearcher() {
+		Book b = null;
 		System.out.printf("SYSTEM :: 도서를 검색합니다.\n1.도서번호 2.ISBN 3.제목 4.저자");
-		
+		int input = MiniUtils.next("입력", Integer.class, i -> i>=1 && i<=4, "1~4 사이의 숫자 입력");
+		switch (input) {
+		case 1: {
+			b = findByBookId(MiniUtils.next("번호 입력", String.class, s -> findByBookId(s) != null, "존재하지 않는 도서번호입니다."));
+			System.out.println("=====검색 결과=====");
+			System.out.println(b);
+			break;
+		}
+		case 2: {
+			b = findByBookId(MiniUtils.next("번호 입력", String.class, s -> findByBookISBN(s) != null, "존재하지 않는 도서번호입니다."));
+			System.out.println("=====검색 결과=====");
+			System.out.println(b);
+			break;
+		}
+		case 3 : {
+			String a = MiniUtils.next("제목 입력", String.class);
+			findByName(a);
+			break;
+		}
+		case 4 :{
+			String a = MiniUtils.next("제목 입력", String.class);
+			findByWriter(a);
+			break;
+		}
+		default:
+			break;
+		}
 	}
 // 1. 검색 메서드 구현(제가 만들었던 셔플 리스트 이외에도 스트링 명령어 사용해서 검색어를 통해 )
 }
