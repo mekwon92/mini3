f41b9839cde56e59f3c723afd251c0e3164f3c70
Sangwon1233 <https://github.com/Sangwon1233/java_basic.git>
2024-09-30 18:22:44 +0900

    카트서비스 및 세일

--- src/cart/Cart.java
+++ src/cart/Cart.java
@@ -1,57 +1,19 @@
 package cart;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import miniBook.Book;
 
 public class Cart {
+	private List<Book> carts = new ArrayList<Book>(); // 장바구니
 
-
-	private String booknum;
-	private int bookPrice;
-	private String user;
-
-	public Cart() {
+	public List<Book> getCarts() {
+		return carts;
 	}
 
-	public Cart (String booknum, int bookprice, int buy, int cancle,int add) {
-
-	
-		this.booknum = booknum;
-		this.bookPrice = bookprice;
-
+	public void setCarts(List<Book> carts) {
+		this.carts = carts;
 	}
-
-	public Cart(Cart cs) {
-		setBooknum(cs.getBooknum());//원래는 학번으로 작동했는데 여기서는 북 넘버로 작동해야할듯?
-	
-	}
-
-//	
-
-
-	public String getBooknum() {
-		return booknum;
-	}
-
-	public void setBooknum(String booknum) {//책 번호
-		this.booknum = booknum;
-	}
-
-	public int getBookPrice() {
-		return bookPrice;
-	}
-
-	public void setBookPrice(int bookPrice) {
-		this.bookPrice = bookPrice;
-	}
-
-	public String getUser() {
-		return user;
-	}
-
-	public void setUser(String user) {
-		this.user = user;
-	}
-
-
 }
 
