fbd8acdb104c937393b6a3fc20707e4d203c19cf
Sangwon1233 <https://github.com/Sangwon1233/java_basic.git>
2024-09-30 15:11:30 +0900

    구매,장바구니 설정 추가

--- src/cart/CartService.java
+++ src/cart/CartService.java
@@ -1,48 +1,59 @@
-//package cart;
-//
-//import java.io.FileInputStream;
-//import java.io.FileNotFoundException;
-//import java.io.FileOutputStream;
-//import java.io.IOException;
-//import java.io.ObjectInputStream;
-//import java.io.ObjectOutputStream;
-//import java.util.ArrayList;
-//import java.util.List;
-//import miniBook.Book;
-//
-//
-//
-//
-//public class CartService {	
-//	
-//	//1.구매 2. 취소 3.장바구니로 이동 4.뒤로 가기 5.종료
-//	private List<Book> carts = new ArrayList<Book>(); //장바구니
-//	private List<Book> back;
-//	//private List<Book> buy;
-//	private List<Book> cancel;
-//	
-//	
-//	
-//
-//	{
-//		try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream("money"))) {
-//			carts = (List<Book>) ois.readObject();
-//		} catch (FileNotFoundException e) {
-////			System.out.println("파일 검색 실패, 초기화 더미 데이터 처리 완료");
-//		} catch (IOException | ClassNotFoundException e) {
-//			e.printStackTrace();//예외 정보 출력
-//		} 
+package cart;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import miniBook.Book;
+import miniBook.BookService;
+
+public class CartService {
+
+	// 일단 출력 보려고 필드에 선언을 해 놓은 상태
+	private List<Book> carts = new ArrayList<Book>(); // 장바구니
+	private List<Book> back;
+	private List<Book> buy;
+	private List<Book> cancel;
+	private List<Book> add;
+
+	{
+		try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("money"))) {// 폴더를 만들어서 그 전에 금액을 저장
+			carts = (List<Book>) ois.readObject();
+		} catch (FileNotFoundException e) {
+//			System.out.println("파일 검색 실패, 초기화 더미 데이터 처리 완료");
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();// 예외 정보 출력
+		}
+
+	}
+
+//	public void buy() { //구매
+//		List<Book> tmpList = new ArrayList<Book>();
+//		(BookService.getBookList().get(findby("책 ID넣기")));
+//		carts.add();
 //		
 //	}
 //	
-//	
-////	
-////	public void buy() { //구매
-////		List<Book> tmpList = new ArrayList<Book>()
-////		(BookService.getBookList().get(findby("")));
-////		carts.add();
-////		
-////	}
+//public void add() {
+//		
+//		String booknum = MiniUtils.next("책 번호를 입력하세요", String.class, n -> findBy(n) == null, "정확한 책을 입력하세요");
+//		//책 번호를 입력하세요 후 입력하면 구매와 뒤로가기 버튼이 나왔으면 좋겠음
+//		
+//		booklist.add() = new cart(Booknum);
+//}
+
+	public void add() {
+		
+		Book Cart = findBy(MiniUtils.next("책 번호", String.class, n -> findBy(n) != null, "입력한 책은 존재하지 않습니다."));
+
+	}
+
+//		
+//		students.add(new Student(no, name, kor, eng, mat));
 //	
 //	public void buy() { //구매
 //		List<Book> bookList = new ArrayList<Book>()
@@ -50,83 +61,71 @@
 //		
 //	}
 //	
-//	public void cancel() { //담은 책 취소
-//		List<Book> bookList = new ArrayList<Book>()
-//		(BookService.getBookList().get(findby("")));
-//		List<Book> bookcancel= null; //취소 할 책을 없애기
-//	}
-//	
-//	public void back() { //뒤돌아가기
-//		List<Book> bookList = new ArrayList<Book>()
-//		(BookService.getBookList().get(findby("")));
-//		
-//	}
-//	
-//
-//	private Book findBy(String buy) {
-//		Book cart = null;
-//		for(int i = 0 ; i < carts.size() ; i++) {
-//			if(carts.get(i).getBookId() == buy) {
-//				cart = carts.get(i);
-//			}
+	public void cancel() { // 담은 책 취소
+
+	}
+
+	public void back() { // 뒤돌아가기
+
+	}
+
+	private Book findBy(String book) {
+		Book cart = null;
+		for (int i = 0; i < carts.size(); i++) {
+			if (carts.get(i).getBookId() == book) {
+				cart = carts.get(i);
+			}
+		}
+		return cart;
+	}
+
+//장바구니 들어왔을때
+	public void cartlist() {
+		// 책 정보
+		int input = MiniUtils.next("1.구매 2.책 삭제 3.추가 및 수량변경", Integer.class, i -> i <= 4 && i >= 1,
+				"1이상 3이하의 값을 입력하세요");
+		List<Book> tmp = null;
+		switch (input) {
+		case 1:
+			System.out.println("구매완료.");
+			tmp = buy;
+			// 여기에 가격이랑 무슨책인지 나왔으면 좋겠음
+			break;
+		case 2:
+			System.out.println("취소합니다.");
+			// 현재 장바구니에 담긴 책 선택 삭제
+			tmp = cancel;
+			break;
+		case 3:
+			System.out.println("책 추가 할 책을 입력해주세요");
+			// 1.책 입력
+			// 2.책 번호를 입력하세요 후 입력하면 다시 이창이뜨면 좋곘음
+			tmp = add;
+
+			break;
+
+		default:
+			System.out.println("????");
+			break;
+		}
+		for (int i = 0; i < tmp.size(); i++) {
+//				System.out.println(students[i]);
+			System.out.println(tmp.get(i));
+		}
+	}
+
+	public void remove() { // 장바구니에 서적 삭제
+		Book cs = findBy(MiniUtils.next("책 번호를 입력하세요", String.class, n -> findBy(n) != null, "입력한 책은 존재하지 않습니다."));
+		carts.remove(cs);
+	}
+
+//	public void save() { //매출 관련
+//		try(ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream("data.ser"))) {
+//			stream.writeObject(students);
+//		} catch (IOException e) {
+//			e.printStackTrace();
 //		}
-//		return cart;
 //	}
-//	
-//	
-//
-////장바구니 들어왔을때
-//		public void  cartlist() {// 구매 조회
-//			//책 정보
-//			int input = MiniUtils.next("1.구매 2.취소 3.뒤로가기", Integer.class, i -> i <= 3 && i >= 1, "1이상 3이하의 값을 입력하세요");
-//			List<Book> tmp = null;
-//			switch (input) {
-//			case 1:
-//				System.out.println("장바구니에 담습니다.");
-//				tmp = buy;
-//				
-//				break;
-//			case 2:
-//				System.out.println("취소합니다.");
-//				
-//				tmp = cancel;
-//				break;
-//			case 3:
-//				System.out.println("목록으로 돌아가기");
-//				tmp = back;
-//
-//				break;
-//			default:
-//				System.out.println("????");
-//				break;
-//			}
-//			for(int i = 0 ; i < tmp.size() ; i++) {
-////				System.out.println(students[i]);
-//				System.out.println(tmp.get(i));
-//			}
-//		}
-//	public void remove() { //장바구니에 서적 삭제
-//		Book cs = findBy(MiniUtils.next("책 번호를 입력하세요", String.class, n -> findBy(n) != null, "입력한 책은 존재하지 않습니다."));
-//		carts.remove(cs);
-//	}
-//	
-//
-////	public void save() { //매출 관련
-////		try(ObjectOutputStream stream = new ObjectOutputStream(new FileOutputStream("data.ser"))) {
-////			stream.writeObject(students);
-////		} catch (IOException e) {
-////			e.printStackTrace();
-////		}
-////	}
-//// 장바구니 재고 같은경우 클론을 복제하여 장바구니에서 취소할 경우 그 원본과 장바구니 재고가 같게 한다	
-//		
-//
-//
-//	
-//}
-//
-//
-//
-//
-//
-//
+// 장바구니 재고 같은경우 클론을 복제하여 장바구니에서 취소할 경우 그 원본과 장바구니 재고가 같게 한다	
+
+}
