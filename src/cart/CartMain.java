198a8be00f0a60aafde71eac77c7fe072dde900b
Sangwon1233 <https://github.com/Sangwon1233/java_basic.git>
2024-10-02 09:44:25 +0900

    헤드 제거

--- src/cart/CartMain.java
+++ src/cart/CartMain.java
@@ -16,63 +16,9 @@ public class CartMain {
 				case 1:
 					cs.cartlist();
 					break;
-//				case 2:
-//					cs.back();
-//					break;
-//				case 3:
-//					cs.add(); //
-//					break;
-<<<<<<< HEAD
-//				}
-//			}
-//			catch (NumberFormatException e) {
-//				System.out.println("정확한 숫자를 입력하세요");
-//			}
-//			catch (RuntimeException e) {
-//				System.out.println(e.getMessage());
-//				e.printStackTrace();
-//			}
-//			
-//		}
-//	}
-//}
-
-import java.util.List;
-
-import miniBook.Book;
-
-public void add(Book a) {
-		a.clone();// 책 복사
-		List<Book> list = cart.getCarts();
-		if (list.contains(a)) {
-
-		}
-		boolean flag = false;
-		Book tmp = null;
-		for (Book book : list) {
-			if (book.getBookId().equals(a.getBookId())) {
-				flag = true;// 조건이 트루일때
-				tmp = book; // 책을 탬에 담는다
-				break;
-			}
-		}
-		// 담으려는 책이 카트에 존재하는가?
-		// 이미 있던 책의 수량 ++
-		if (flag) {
-			tmp.increaseBookCount();
-		}
-		// add(클론대상)
-		else {
-			list.add(a.clone());
-		}
-	}
-=======
-				case 4:
-					cs.remove();
-//					break;
-//				case 5:
-//					System.out.println("책 구매을 종료합니다");
-//					cs.cancel();				
+					
+				case 5:
+					System.out.println("bye");
 					return;
 				default:
 					break;
@@ -88,5 +34,8 @@ public void add(Book a) {
 			
 		}
 	}
+		
+	
 }
->>>>>>> 3baa2fa5fb8d106f6b804d3d211de35f81a34c1d
+			
+
