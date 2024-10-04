c2a390e668c6f791d8f6a64871cd474ac2f351cc
Sangwon1233 <https://github.com/Sangwon1233/java_basic.git>
2024-10-04 16:21:53 +0900

    오류 주석

--- src/miniBook/NaverApiCaller.java
+++ src/miniBook/NaverApiCaller.java
@@ -12,8 +12,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+//import org.json.JSONArray;
+//import org.json.JSONObject;
 import miniBook.BookService.*;
 
 public class NaverApiCaller {
@@ -21,72 +21,72 @@ public class NaverApiCaller {
 	String secret = "m8VlYwqi8t";
 	static int counter;
 
-	public List<Book> call(String query) {
-		List<Book> list = new ArrayList<Book>();
-		int respCode = 0;
-		InputStream is = null;
-		try {
-			query = URLEncoder.encode(query, "utf-8");
-			String url = "https://openapi.naver.com/v1/search/book.json?query=" + query + "&display=100";
+//	public List<Book> call(String query) {
+//		List<Book> list = new ArrayList<Book>();
+//		int respCode = 0;
+//		InputStream is = null;
+//		try {
+//			query = URLEncoder.encode(query, "utf-8");
+//			String url = "https://openapi.naver.com/v1/search/book.json?query=" + query + "&display=100";
+//
+//			HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+//			conn.setRequestMethod("GET");
+//			conn.setRequestProperty("X-Naver-Client-Id", clientID);
+//			conn.setRequestProperty("X-Naver-Client-Secret", secret);
+//			respCode = conn.getResponseCode(); // 200
+//			is = conn.getInputStream();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		System.out.println(respCode + "수신 성공");
+//		if (respCode == HttpURLConnection.HTTP_OK) {
+//			StringBuilder sb = new StringBuilder();
+//			try (BufferedReader br = new BufferedReader(new InputStreamReader(is));) {
+//				String tmp = null;
+//				while (null != (tmp = br.readLine())) {
+//					sb.append(tmp);
+//				}
+//				list = StringJSON2List(sb.toString());
+////				oos.writeObject(list);
+//			} catch (Exception e) {
+//				e.printStackTrace();
+//			}
+//		}
+//		return list;
+//
+//	}
 
-			HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
-			conn.setRequestMethod("GET");
-			conn.setRequestProperty("X-Naver-Client-Id", clientID);
-			conn.setRequestProperty("X-Naver-Client-Secret", secret);
-			respCode = conn.getResponseCode(); // 200
-			is = conn.getInputStream();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		System.out.println(respCode + "수신 성공");
-		if (respCode == HttpURLConnection.HTTP_OK) {
-			StringBuilder sb = new StringBuilder();
-			try (BufferedReader br = new BufferedReader(new InputStreamReader(is));) {
-				String tmp = null;
-				while (null != (tmp = br.readLine())) {
-					sb.append(tmp);
-				}
-				list = StringJSON2List(sb.toString());
-//				oos.writeObject(list);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return list;
+//	public List<Book> StringJSON2List(String str) {
+//		JSONObject jObject = new JSONObject(str);
+//		JSONArray jArray = (JSONArray) jObject.get("items");
+//		List<Book> list = new ArrayList<>();
+//
+//		for (int i = 0; i < jArray.length(); i++) {
+//			JSONObject item = (JSONObject) jArray.get(i);
+//			String title = item.getString("title");
+//			String author = item.getString("author");
+//			String isbn = item.getString("isbn");
+//			String publisher = item.getString("publisher");
+//			String description = item.getString("description");
+//			Integer discount = item.getInt("discount");
+//
+//			Book book = new Book(String.format("%04d", ++counter), title, author, publisher, isbn, description,
+//					discount, 1000 , false, false);
+//			list.add(book);
+//			System.out.println(list);
+//		}
+//		return list;
+//	}
 
-	}
-
-	public List<Book> StringJSON2List(String str) {
-		JSONObject jObject = new JSONObject(str);
-		JSONArray jArray = (JSONArray) jObject.get("items");
-		List<Book> list = new ArrayList<>();
-
-		for (int i = 0; i < jArray.length(); i++) {
-			JSONObject item = (JSONObject) jArray.get(i);
-			String title = item.getString("title");
-			String author = item.getString("author");
-			String isbn = item.getString("isbn");
-			String publisher = item.getString("publisher");
-			String description = item.getString("description");
-			Integer discount = item.getInt("discount");
-
-			Book book = new Book(String.format("%04d", ++counter), title, author, publisher, isbn, description,
-					discount, 1000 , false, false);
-			list.add(book);
-			System.out.println(list);
-		}
-		return list;
-	}
-
-	public static void main(String[] args) throws Exception {
-		NaverApiCaller nac = new NaverApiCaller();
-		String[] queries = { "전래동화", "서양고전" };
-		List<Book> results = new ArrayList<Book>();
-		Arrays.asList(queries).forEach(s -> results.addAll(nac.call(s)));
-
-		results.forEach(System.out::println);
-		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("data.ser"));
-		oos.writeObject(results);
-		oos.close();
-	}
+//	public static void main(String[] args) throws Exception {
+//		NaverApiCaller nac = new NaverApiCaller();
+//		String[] queries = { "전래동화", "서양고전" };
+//		List<Book> results = new ArrayList<Book>();
+//		Arrays.asList(queries).forEach(s -> results.addAll(nac.call(s)));
+//
+//		results.forEach(System.out::println);
+//		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("data.ser"));
+//		oos.writeObject(results);
+//		oos.close();
+//	}
 }
