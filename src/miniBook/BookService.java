4219db7989f1cb83cdd19125fd9b708f204a8975
DaHnDell <khanman1208@gmail.com>
2024-10-07 20:27:36 +0900

    마지막 커밋

--- src/miniBook/BookService.java
+++ src/miniBook/BookService.java
@@ -14,20 +14,19 @@ import miniCart.CartService;
 import miniCustomer.CustomerService;
 import miniCustomer.MiniUtils;
 
-
 /**
  * 서적 서비스부 싱글턴 적용할 것
  * 
  * @author HHJ, KHM
  */
 public class BookService {
-// 싱글턴 적용
+// 	싱글턴 적용
 //	필드 초기화
-	private static BookService BookService = new BookService();
 	private Calendar calender = Calendar.getInstance();
-	private int month = calender.get(Calendar.MONTH)+ 1;
+	private int month = calender.get(Calendar.MONTH) + 1;
 	SimpleDateFormat sdf = new SimpleDateFormat("yyyy년 MM월 dd일");
 	CartService cartService = CartService.getCartService();
+	private static BookService BookService = new BookService();
 
 	private BookService() {
 		// TODO Auto-generated constructor stub
@@ -63,9 +62,10 @@ public class BookService {
 		bookEvent(bookList);
 		boolean flag = true;
 		System.out.print(""
-				+ "======================================================================\n"
-				+ "SYSTEM :: 얼라들의 알라딘에서 무엇이든 검색하세요! 현재 날짜는" + sdf.format(calender.getTime()) + "입니다. :: \n"
-				+ "======================================================================\n" );
+				+ "=================================================================================================\n"
+				+ "SYSTEM :: 얼라들의 알라딘, < 얼라딘 > 에서 무엇이든 검색하세요! 현재 날짜는 >> " + sdf.format(calender.getTime())
+				+ "입니다. :: \n"
+				+ "=================================================================================================\n");
 		String[] categories = { "도서번호", "ISBN", "제목", "저자", "전체", "뒤로", "장바구니" };
 		while (flag) {
 			System.out.println("SYSTEM :: 도서를 검색합니다.");
@@ -73,7 +73,7 @@ public class BookService {
 				System.out.printf(" %d.%s", (i + 1), categories[i]);
 			}
 			System.out.println();
-			int input = MiniUtils.next(" SYSTEM :: 카테고리 입력", Integer.class, i -> i >= 1 && i <= 7, "1~7 사이의 숫자 입력");
+			int input = MiniUtils.next(" SYSTEM :: 카테고리 입력", Integer.class, i -> i >= 1 && i <= 7, "1~7 사이의 숫자 입력 :: ");
 			switch (input) {
 			case 1:
 			case 2:
@@ -121,18 +121,19 @@ public class BookService {
 			return;
 		}
 		if (books != null && !books.isEmpty()) {
-			System.out.println("=========================== 검색 결과 ===========================");
+			System.out.println("============================== 검색 결과 ==============================");
 			for (int i = 0; i < books.size(); i++) {
 				System.out.print(" :: " + (i + 1) + " :: ");
 				System.out.println(books.get(i));
 			}
-			System.out.println("SYSTEM :: 출력이 완료되었습니다.");
+			System.out.println("SYSTEM :: 출력이 완료되었습니다. :: ");
 			int sizebook = books.size();
-			int input2 = MiniUtils.next("번호로 책을 골라주세요", Integer.class, s -> s >= 1 && s <= sizebook, "SYSTEM :: INPUT ERROR");
+			int input2 = MiniUtils.next("SYSTEM :: 번호로 책을 골라주세요", Integer.class, s -> s >= 1 && s <= sizebook,
+					"SYSTEM :: INPUT ERROR :: ");
 			System.out.println("SYSTEM :: " + books.get(input2 - 1).getBookName() + "의 상세정보를 로드합니다.");
 			showBookDetails(books.get(input2 - 1));
 		} else {
-			System.err.println("SYSTEM :: 검색 결과가 없습니다.");
+			System.err.println("SYSTEM :: 검색 결과가 없습니다. :: ");
 		}
 	}
 
@@ -144,9 +145,9 @@ public class BookService {
 	public void printBooks() {
 		List<Book> pBook = new ArrayList<>(bookList);
 		Collections.shuffle(pBook);
-		System.out.println("SYSTEM :: 도서 리스트를 출력합니다.");
+		System.out.println("SYSTEM :: 도서 리스트를 출력합니다. :: ");
 		pBook.forEach(x -> System.out.print(x + "\n"));
-		System.out.println("SYSTEM :: 출력이 완료되었습니다.");
+		System.out.println("SYSTEM :: 출력이 완료되었습니다. :: ");
 	}
 
 	/**
@@ -184,7 +185,7 @@ public class BookService {
 			}
 		}
 		return tmp;
-		
+
 	}
 
 	/**
@@ -228,19 +229,19 @@ public class BookService {
 		System.out.println("*소개 : " + bookList.get(Integer.parseInt(a.getBookId())).getBookDetail() + "\n | *정가 : "
 				+ a.getBookPrice() + " |" + "1.장바구니 2.뒤로가기");
 		CartService cs = CartService.getCartService();
-		int key = MiniUtils.next("입력", Integer.class, i -> i >= 1 && i <= 2, "SYSTEM :: INPUT ERROR");
+		int key = MiniUtils.next("입력", Integer.class, i -> i >= 1 && i <= 2, "SYSTEM :: INPUT ERROR :: ");
 		switch (key) {
 		case 1: {
 			if (a.getBookPrice() == 0) {
-				System.out.println("SYSTEM :: 절판된 상품입니다. 구매 불가");
+				System.out.println("SYSTEM :: 절판된 상품입니다. 구매가 불가합니다. 검색 화면으로 돌아갑니다. :: ");
 				break;
 			}
 			cs.add(a);
-			System.out.println("SYSTEM :: 장바구니에 상품이 담겼습니다.");
+			System.out.println("SYSTEM :: 장바구니에 상품이 담겼습니다. :: ");
 			cs.printCart();
 		}
 		case 2: {
-			System.out.println("SYSTEM :: 초기 화면으로 돌아갑니다.");
+			System.out.println("SYSTEM :: 초기 화면으로 돌아갑니다. :: ");
 			break;
 		}
 		}
@@ -286,8 +287,8 @@ public class BookService {
 		boolean b1 = false;
 		boolean b2 = false;
 
-		bookList.add(
-				new Book(bookId, bookName, bookWriter, bookPublisher, ISBookNum, bookDetail, bookPrice, bookCount, b1, b2));
+		bookList.add(new Book(bookId, bookName, bookWriter, bookPublisher, ISBookNum, bookDetail, bookPrice, bookCount,
+				b1, b2));
 	}
 
 	/**
@@ -314,8 +315,8 @@ public class BookService {
 		b.setBookCount(bookCount);
 		boolean b1 = false;
 		boolean b2 = false;
-		bookList.add(
-				new Book(bookId, bookName, bookWriter, bookPublisher, ISBookNum, bookDetail, bookPrice, bookCount, b1, b2));
+		bookList.add(new Book(bookId, bookName, bookWriter, bookPublisher, ISBookNum, bookDetail, bookPrice, bookCount,
+				b1, b2));
 	}
 
 	/**
@@ -330,7 +331,7 @@ public class BookService {
 
 	private Book findBy(String bookId) {
 		Book book = null;
-		for (int i = 0; i < bookList.size(); i++) { 
+		for (int i = 0; i < bookList.size(); i++) {
 			if (bookList.get(i).getBookId() == bookId) {
 				book = bookList.get(i);
 			}
@@ -338,14 +339,14 @@ public class BookService {
 		return book;
 	}
 
-	
 	public void bookEvent(List<Book> listTarget) {
-		int ran = (int)(Math.random() * listTarget.size());
+		int ran = (int) (Math.random() * listTarget.size());
 		CustomerService customer = CustomerService.getInstance();
 		String name = customer.getLoggedInUser().getName();
 		Book book = bookList.get(ran);
-		System.out.println(" SYSTEM :: " + name + " 님, 어서오세요! 아래의 이벤트들을 확인해 보세요!");
-		System.out.println(month + " 월의 [ 추천 도서 :: < " + book.getBookName() + " > :: ]  " + month + " 월의 [ 얼라딘터뷰 :: < " + book.getBookWriter() + " 작가 > :: ]");
+		System.out.println(" SYSTEM :: " + name + " 님, 어서오세요! 아래의 이벤트들을 확인해 보세요! :: ");
+		System.out.println(month + " 월의 [ 추천 도서 :: < " + book.getBookName() + " > :: ]  " + month + " 월의 [ 얼라딘터뷰 :: < "
+				+ book.getBookWriter() + " 작가 > :: ]");
 	}
-	
+
 }
